0500b8e2fb1fbc10d45468510e8889a19dc47689
DilMuradMastoi <webdeveloper1234567890003@gmail.com>
2026-08-08 13:44:55 +0500

    api connected

--- introduction/src/config/firebaseconfig.js
+++ introduction/src/config/firebaseconfig.js
@@ -4,7 +4,7 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+apiKey: "AIzaSyBnu5KnU2dFV0y-jmZ4QqLxx7GupJb_XjU",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
